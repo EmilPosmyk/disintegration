f18d5c01be4ca4ed0985f1b6824cf0f9675e115d
EmilPosmyk <emil.posmyk@gmail.com>
2021-08-25 10:18:11 +0200

    minor changes

--- disintegration/lib.py
+++ disintegration/lib.py
@@ -1,2 +1,13 @@
+def isPrime(input_number):
+    for n in range(2, input_number):
+        if (input_number % n) == 0:
+            return False
+    return True
+
+
+def define_number_of_trials(num):
+    for num in range(2, num, 1):
+        print(f'{num} {isPrime(num)}')
+
 def try_me():
     return 'successful disintegration'
