340be27ea79e39ad32a24bbf92c25eb791491c6e
EmilPosmyk <emil.posmyk@gmail.com>
2021-08-25 10:20:40 +0200

    bump version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('requirements.txt') as f:
 requirements = [x.strip() for x in content if 'git+' not in x]
 
 setup(name='disintegration',
-      version="0.0.2",
+      version="0.0.3",
       author='Disintegrated persona n.g.',
       description="Reintegrating disintegrated ",
       packages=find_packages(),
